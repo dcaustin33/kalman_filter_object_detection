0c5efae064853fa1be726bd0284eb7ecd23ec696
dcaustin33 <dcaustin33@gmail.com>
2024-10-31 13:35:30 -0400

    First commit

---
+++ object_tracking/kalman_filter/fake_data.py
@@ -0,0 +1,33 @@
+import random
+
+import numpy as np
+
+
+def generate_fake_data(steps: int = 100) -> list:
+    """Generates fake kalman data used for testing"""
+    random.seed(42)
+
+    position_0 = (10, 10)
+    width = 2
+    height = 2
+
+    steps = 100
+
+    random.seed(42)
+    
+    # boxes will be in cx, cy, h, w format
+    all_boxes = []
+    current_position = position_0
+    for i in range(steps):
+        velocity_x = random.randint(1, 6)
+        velocity_y = random.randint(1, 4)
+        width_noise = 0 #random.randint(-2, 2)
+        height_noise = 0 #random.randint(-2, 2)
+        
+        current_position = [current_position[0] + velocity_x, current_position[1] + velocity_y]
+        current_width = width + width_noise
+        current_height = height + height_noise
+
+        all_boxes.append([current_position[0], current_position[1],  current_width, current_height])
+        
+    return all_boxes
---
+++ object_tracking/kalman_filter/schema.py
@@ -0,0 +1,89 @@
+"""
+Implementation of a 1D Kalman Filter for tracking a single object
+
+Assumptions:
+    - There is no Q noise
+    - We can independently track x, y so we only need to implement a 1D version
+"""
+
+from dataclasses import dataclass
+
+import numpy as np
+
+
+@dataclass
+class KalmanStateVector:
+    x: float
+    y: float
+    width: float
+    height: float
+    vx: float
+    vy: float
+    vw: float
+    vh: float
+
+
+class KalmanStateVector1D:
+    x: float
+    vx: float
+    cov: np.ndarray
+
+    def __init__(self, x: float, vx: float) -> None:
+        self.x = x
+        self.vx = vx
+        self.cov = None
+
+    def initialize_covariance(self, noise_std: float) -> None:
+        self.cov = np.array([[noise_std**2, 0], [0, noise_std**2]])
+
+    def predict_next_state(self, dt: float) -> None:
+        self.x = self.x + self.vx * dt
+        self.predict_next_covariance(dt)
+
+    def predict_next_covariance(self, dt: float) -> None:
+        # for right now we are assuming no Q noise
+        self.cov[0][0] = self.cov[0][0] + self.cov[1][1] * dt**2
+        self.cov[0][1] = self.cov[0][1] + self.cov[1][1] * dt
+        self.cov[1][0] = self.cov[1][0] + self.cov[1][1] * dt
+        self.cov[1][1] = self.cov[1][1]
+        
+    def __add__(self, other: np.ndarray) -> np.ndarray:
+        return np.array([self.x + other[0][0], self.vx + other[1][0]])
+
+
+class Kalman1DTracker:
+
+    def __init__(
+        self,
+        state: KalmanStateVector1D,
+        state_noise_std: float,
+        measurement_noise_std: float,
+    ) -> None:
+        self.state = state
+        self.state.initialize_covariance(state_noise_std)
+        self.predicted_state = None
+        self.measured_state = None
+        self.measurement_noise_std = measurement_noise_std
+        self.previous_states = []
+
+    def predict(self, dt: float) -> None:
+        # may need to make sure this is copied and not just a reference
+        self.previous_states.append(self.state)
+        self.state.predict_next_state(dt)
+
+    def update(self, measurement: float, dt: float = 1) -> None:
+        self.predict(dt=1)
+        self.measured_state = measurement
+
+        innovation = self.measured_state - self.state.x
+        gain_first_term = 1 / (
+            self.state.cov[0][0] + self.measurement_noise_std**2
+        )
+        gain_second_term = np.array(
+            [self.state.cov[0][0], self.state.cov[0][1]]
+        ).reshape(-1, 1)
+        gain = gain_first_term * gain_second_term
+
+        new_state = self.state + gain * innovation
+        self.state.x = new_state[0]
+        self.state.vx = new_state[1]
